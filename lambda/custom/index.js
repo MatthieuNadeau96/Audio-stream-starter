e90e7162152418cabbd22cc30712a72a7f5d8290
Matthieu <matthieunadeau96@gmail.com>
2018-06-19 16:38:18 -0400

    first commit

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,11 +2,29 @@
 
 var Alexa = require('alexa-sdk');
 
+//////////////////Dynamo Stuff///////////////////
+
+const AWSregion = 'us-east-1';  // us-east-1
+
+const params = {
+    TableName: 'audioSounds',
+    Key:{ "location": 'Forest'  }
+};
+
+// AWS.config.update({
+//     region: AWSregion
+// });
+
+
+
+
+////////////////////////////////////////////////
+
 var streamInfo = {
   title: 'Audio Stream Starter',
   subtitle: 'A starter template for an Alexa audio streaming skill.',
   cardContent: "Get more details at: https://skilltemplates.com",
-  url: 'https://streaming.radionomy.com/RadioXUS?lang=en-US&appName=iTunes.m3u',
+  url: 'https://s3.amazonaws.com/audio-players/Chill-Pill-sample.mp3',
   image: {
     largeImageUrl: 'https://s3.amazonaws.com/cdn.dabblelab.com/img/alexa-card-lg.png',
     smallImageUrl: 'https://s3.amazonaws.com/cdn.dabblelab.com/img/alexa-card-sm.png'
@@ -29,7 +47,17 @@ var handlers = {
     this.emit('PlayStream');
   },
   'PlayStream': function() {
-    this.response.speak('Enjoy.').audioPlayerPlay('REPLACE_ALL', streamInfo.url, streamInfo.url, null, 0);
+    // this.response.speak('Enjoy.').audioPlayerPlay('REPLACE_ALL', streamInfo.url, streamInfo.url, null, 0);
+    readDynamoItem(params, myResult=>{
+        var say = '';
+
+        say = myResult;
+
+        say = 'The answer is: ' + myResult;
+        this.response.speak(say);
+        this.emit(':responseReady');
+    });
+
     this.emit(':responseReady');
   },
   'AMAZON.HelpIntent': function() {
@@ -120,4 +148,26 @@ var audioEventHandlers = {
     this.response.audioPlayerClearQueue('CLEAR_ENQUEUED');
     this.emit(':responseReady');
   }
+}
+
+function readDynamoItem(params, callback) {
+
+    var AWS = require('aws-sdk');
+    AWS.config.update({region: AWSregion});
+
+    var docClient = new AWS.DynamoDB.DocumentClient();
+
+    console.log('reading item from DynamoDB table');
+
+    docClient.get(params, (err, data) => {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+
+            callback(data.Item);
+
+        }
+    });
+
 }
