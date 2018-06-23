fe4c95776ff7242891ae653cb85d8bfd71dace9f
Matthieu <matthieunadeau96@gmail.com>
2018-06-23 16:28:57 -0400

    added the read function

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,173 +1,97 @@
-'use strict';
 
-var Alexa = require('alexa-sdk');
-
-//////////////////Dynamo Stuff///////////////////
+// 1. Text strings =====================================================================================================
+//    Modify these strings and messages to change the behavior of your Lambda function
 
 const AWSregion = 'us-east-1';  // us-east-1
 
 const params = {
-    TableName: 'audioSounds',
-    Key:{ "location": 'Forest'  }
+TableName: 'audioSounds',
+Key:{ "location": 'Forest', "planet": "Earth" }
 };
 
-// AWS.config.update({
-//     region: AWSregion
-// });
+// TODO: now I need to have the location and planet be in dialog so that the user can say where they want to go
 
+// TODO: I also need to bring back the audio stream starter template and implement the dynamo read function so that the user can active the specific sound urls from my S3 bucket
 
+// 2. Skill Code =======================================================================================================
 
+const Alexa = require('alexa-sdk');
+const AWS = require('aws-sdk');
 
-////////////////////////////////////////////////
+AWS.config.update({
+region: AWSregion
+});
 
-var streamInfo = {
-  title: 'Audio Stream Starter',
-  subtitle: 'A starter template for an Alexa audio streaming skill.',
-  cardContent: "Get more details at: https://skilltemplates.com",
-  url: 'https://s3.amazonaws.com/audio-players/Chill-Pill-sample.mp3',
-  image: {
-    largeImageUrl: 'https://s3.amazonaws.com/cdn.dabblelab.com/img/alexa-card-lg.png',
-    smallImageUrl: 'https://s3.amazonaws.com/cdn.dabblelab.com/img/alexa-card-sm.png'
-  }
+exports.handler = function(event, context, callback) {
+var alexa = Alexa.handler(event, context);
+
+alexa.appId = 'amzn1.ask.skill.54254bd5-ee38-4b22-b662-325a632062c6';
+// alexa.dynamoDBTableName = 'YourTableName'; // creates new table for session.attributes
+
+alexa.registerHandlers(handlers);
+alexa.execute();
 };
 
-exports.handler = (event, context, callback) => {
-  var alexa = Alexa.handler(event, context, callback);
+const handlers = {
+'LaunchRequest': function () {
+    this.response.speak('welcome to audio stream starter.  ask me a yes or no question.').listen('try again');
+    this.emit(':responseReady');
+    // TODO: I need to change up the speak response
+},
 
-  alexa.registerHandlers(
-    handlers,
-    audioEventHandlers
-  );
+'MyIntent': function () {
 
-  alexa.execute();
-};
+    // var MyQuestion = this.event.request.intent.slots.MyQuestion.value;
+    // console.log('MyQuestion : ' + MyQuestion);
 
-var handlers = {
-  'LaunchRequest': function() {
-    this.emit('PlayStream');
-  },
-  'PlayStream': function() {
-    // this.response.speak('Enjoy.').audioPlayerPlay('REPLACE_ALL', streamInfo.url, streamInfo.url, null, 0);
     readDynamoItem(params, myResult=>{
         var say = '';
 
         say = myResult;
 
-        say = 'The answer is: ' + myResult;
-        this.response.speak(say);
+        say = 'The answer is: ' + myResult; // TODO: I need to change this
+        this.response.speak(say).listen('try again');
         this.emit(':responseReady');
+
     });
 
+},
+'AMAZON.HelpIntent': function () {
+    this.response.speak('ask me a yes or no question.').listen('try again');
     this.emit(':responseReady');
-  },
-  'AMAZON.HelpIntent': function() {
-    // skill help logic goes here
+},
+'AMAZON.CancelIntent': function () {
+    this.response.speak('Goodbye!');
     this.emit(':responseReady');
-  },
-  'SessionEndedRequest': function() {
-    // no session ended logic needed
-  },
-  'ExceptionEncountered': function() {
-    console.log("\n---------- ERROR ----------");
-    console.log("\n" + JSON.stringify(this.event.request, null, 2));
-    this.callback(null, null)
-  },
-  'Unhandled': function() {
-    this.response.speak('Sorry. Something went wrong.');
+},
+'AMAZON.StopIntent': function () {
+    this.response.speak('Goodbye!');
     this.emit(':responseReady');
-  },
-  'AMAZON.NextIntent': function() {
-    this.response.speak('This skill does not support skipping.');
-    this.emit(':responseReady');
-  },
-  'AMAZON.PreviousIntent': function() {
-    this.response.speak('This skill does not support skipping.');
-    this.emit(':responseReady');
-  },
-  'AMAZON.PauseIntent': function() {
-    this.emit('AMAZON.StopIntent');
-  },
-  'AMAZON.CancelIntent': function() {
-    this.emit('AMAZON.StopIntent');
-  },
-  'AMAZON.StopIntent': function() {
-    this.response.speak('Okay. I\'ve stopped the stream.').audioPlayerStop();
-    this.emit(':responseReady');
-  },
-  'AMAZON.ResumeIntent': function() {
-    this.emit('PlayStream');
-  },
-  'AMAZON.LoopOnIntent': function() {
-    this.emit('AMAZON.StartOverIntent');
-  },
-  'AMAZON.LoopOffIntent': function() {
-    this.emit('AMAZON.StartOverIntent');
-  },
-  'AMAZON.ShuffleOnIntent': function() {
-    this.emit('AMAZON.StartOverIntent');
-  },
-  'AMAZON.ShuffleOffIntent': function() {
-    this.emit('AMAZON.StartOverIntent');
-  },
-  'AMAZON.StartOverIntent': function() {
-    this.response.speak('Sorry. I can\'t do that yet.');
-    this.emit(':responseReady');
-  },
-  'PlayCommandIssued': function() {
-
-    if (this.event.request.type === 'IntentRequest' || this.event.request.type === 'LaunchRequest') {
-      var cardTitle = streamInfo.subtitle;
-      var cardContent = streamInfo.cardContent;
-      var cardImage = streamInfo.image;
-      this.response.cardRenderer(cardTitle, cardContent, cardImage);
-    }
-
-    this.response.speak('Enjoy.').audioPlayerPlay('REPLACE_ALL', streamInfo.url, streamInfo.url, null, 0);
-    this.emit(':responseReady');
-  },
-  'PauseCommandIssued': function() {
-    this.emit('AMAZON.StopIntent');
-  }
 }
+};
 
-var audioEventHandlers = {
-  'PlaybackStarted': function() {
-    this.emit(':responseReady');
-  },
-  'PlaybackFinished': function() {
-    this.emit(':responseReady');
-  },
-  'PlaybackStopped': function() {
-    this.emit(':responseReady');
-  },
-  'PlaybackNearlyFinished': function() {
-    this.response.audioPlayerPlay('REPLACE_ALL', streamInfo.url, streamInfo.url, null, 0);
-    this.emit(':responseReady');
-  },
-  'PlaybackFailed': function() {
-    this.response.audioPlayerClearQueue('CLEAR_ENQUEUED');
-    this.emit(':responseReady');
-  }
-}
+//    END of Intent Handlers {} ========================================================================================
+// 3. Helper Function  =================================================================================================
+
 
 function readDynamoItem(params, callback) {
 
-    var AWS = require('aws-sdk');
-    AWS.config.update({region: AWSregion});
+var AWS = require('aws-sdk');
+AWS.config.update({region: AWSregion});
 
-    var docClient = new AWS.DynamoDB.DocumentClient();
+var docClient = new AWS.DynamoDB.DocumentClient();
 
-    console.log('reading item from DynamoDB table');
+console.log('reading item from DynamoDB table');
 
-    docClient.get(params, (err, data) => {
-        if (err) {
-            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+docClient.get(params, (err, data) => {
+    if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
 
-            callback(data.Item);
+        callback(data.Item.url);  // this particular row has an attribute called message
 
-        }
-    });
+    }
+});
 
 }
